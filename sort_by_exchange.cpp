6d63dcc55ad445f6960649c60478248a33e12cb8
xjq0 <724360863@qq.com>
2019-10-27 20:36:17 +0800

    sort by exchange

--- sort_by_exchange.cpp
+++ sort_by_exchange.cpp
@@ -1,19 +1,19 @@
-//‰∫§Êç¢ÊéíÂ∫è
+//Ωªªª≈≈–Ú
 #include <stdio.h>
 int main() {
-  int x[3] = {5, 2, 1}, i;
+  int x[3],i,n;
   while(1) {
     scanf("%d %d %d", &x[0], &x[1], &x[2]);
-    for(i=0;i<2;i++) {
-      if( x[i] > x[i+1]) {
-  	    int temp = x[i];
-  	    x[i] = x[i+1];
-  	    x[i+1] = temp;
-      }
-      i = i + 1;
-      i = i - 1;
+    for(n=0;n<2;n++){
+        for(i=0;i<2;i++) {
+            if( x[i] > x[i+1]) {
+  	            int temp = x[i];
+  	            x[i] = x[i+1];
+  	            x[i+1] = temp;
+            }
+       }
     }
-    printf(" %d, %d, %d", x[0], x[1], x[2]);
+    printf("%d %d %d", x[0], x[1], x[2]);
   }
   return 0;
 }
